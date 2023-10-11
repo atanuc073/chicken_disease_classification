f19b55c72b972db22fafbee54739a0dc81458578
atanuc073 <atanuc073@gmail.com>
2023-10-12 01:46:49 +0530

    prepaare base model modular code'

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
 from cnnClassifier.utils.common import read_yaml,create_directories
 CONFIG_FILE_PATH=Path("config/config.yaml")
 PARAMS_FILE_PATH=Path("params.yaml")
@@ -9,7 +9,7 @@ class ConfigurationManager:
         config_filepath = CONFIG_FILE_PATH,
         params_filepath = PARAMS_FILE_PATH):
         self.config=read_yaml(config_filepath)
-        # self.params=read_yaml(params_filepath)
+        self.params=read_yaml(params_filepath)
         create_directories([self.config.artifacts_root])
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
@@ -24,3 +24,21 @@ class ConfigurationManager:
             kaggle_path=config.kaggle_path
         )
         return data_ingestion_config
+    
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config=self.config.prepare_base_model
+        create_directories([config.root_dir])
+
+        prepare_base_model_config=PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+        return prepare_base_model_config
+    
