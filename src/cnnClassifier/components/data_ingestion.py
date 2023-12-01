4175d8f2b5e12eb4be07922418c0b436a6a8aecc
atanuc073 <atanuc073@gmail.com>
2023-12-01 20:01:37 +0530

    make good structure of the data

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -4,6 +4,9 @@ import zipfile
 from cnnClassifier import logger
 from cnnClassifier.entity.config_entity import DataIngestionConfig
 from cnnClassifier.utils.common import get_size
+import shutil
+import random
+from tqdm import tqdm
 
 import os
 import zipfile
@@ -17,6 +20,7 @@ class DataIngestion:
         dataset_slug = self.config.kaggle_data
         destination_folder = self.config.local_data_file
         data_name=destination_folder+dataset_slug.split('/')[-1]+".zip"
+        print(data_name)
         print(not os.path.exists(data_name))
         if not os.path.exists(data_name):
             # Create the command to download the dataset into the specified folder
@@ -27,7 +31,8 @@ class DataIngestion:
             os.system(command)
             logger.info(f"{dataset_slug} downloaded ! ")
         else:
-            logger.info(f"The file is already exists of size : {get_size(data_name)}")
+            logger.info(f"The file is already exists of size :")
+
     def extract_zip_file(self):
         unzip_path = self.config.unzip_dir
         dataset_slug = self.config.kaggle_data
@@ -39,5 +44,28 @@ class DataIngestion:
             logger.info(f"file has been successfully exxtracted .")
         except:
             logger.info("there is some error while extracting the files")
+
+    def process_data(self,images_des="artifacts\data_ingestion\Train\\",data_des="artifacts\Processed_data\\"):
+
+        split="train\\"
+        for l in tqdm(os.listdir(images_des)):
+            clas=l.split(".")[0]
+            if random.random()<0.8:
+                split="train\\"
+            else:
+                split="test\\"
+            copy_des=data_des+split+clas
+
+
+            if not os.path.exists(copy_des):
+                # Create the folder
+                os.makedirs(copy_des)
+                print(f"Folder '{copy_des}' created successfully.")
+
+
+            try :
+                shutil.move(images_des+l,copy_des)
+            except:
+                print("faced_error")
 
 
--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -14,6 +14,10 @@ class DataIngestionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+        print(
+            'hello'
+        )
+        data_ingestion.process_data()
 
 
 if __name__ == '__main__':
